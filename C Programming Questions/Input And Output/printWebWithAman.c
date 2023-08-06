486cb23eaf4392ade29ba5b4ec175e827ee6302e
aman <webwithaman06@gmail.com>
2023-08-06 12:24:14 +0530

    commiting first program of Input And Output

---
+++ C Programming Questions/Input And Output/printWebWithAman.c
@@ -0,0 +1,16 @@
+// C Program to Print WebWithAman On Screen
+
+//Header files
+#include<stdio.h>
+#include<conio.h>
+
+
+// Main Function Start
+int main(){
+
+    printf("WebWithAman");  // printf() Function is Used to Print Output On Screen
+
+    getch(); 
+    return 0;
+}
+// Main Function End
