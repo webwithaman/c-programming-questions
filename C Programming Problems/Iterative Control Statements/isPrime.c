843ed1583f16e9b32e73cd3ec6fd64e33ee23b89
aman <webwithaman06@gmail.com>
2023-08-14 21:38:07 +0530

    some more programs on loop

--- C Programming Problems/Iterative Control Statements/isPrime.c
+++ C Programming Problems/Iterative Control Statements/isPrime.c
@@ -1,9 +1,11 @@
 // C Program to Check Whether A Given Number is Prime or Not
 
+
 // Header files
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
+
 
 // Main Function Start
 int main()
@@ -12,23 +14,23 @@ int main()
     printf("\nEnter A Number to Check Whether is Prime or Not => ");
     scanf("%d", &num);
 
-    int isPrime = num < 2 ? 0 : 1;
+
+    int isPrime = num < 2 ? 0 : 1; // if isPrime = 1 then It is Assumed that the Number is Prime else Not Prime
 
     // 1st Approach
     for (int i = 2; i <= sqrt(num); i++)
     {
-        if (num % i == 0)
+        if (num % i == 0) // we can also write this if(!(num % i))
         {
             isPrime = 0;
             break;
         }
     }
 
-
     // 2nd Approach
     // for (int i = 2; i <= num / 2; i++)
     // {
-    //     if (num % i == 0)
+    //     if (num % i == 0) // we can also write this if(!(num % i))
     //     {
     //         isPrime = 0;
     //         break;
---
+++ C Programming Problems/Iterative Control Statements/lcm.c
@@ -0,0 +1,52 @@
+// C Program to Find LCM of Two Numbers
+
+// Header files
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// Main Function Start
+int main()
+{
+    int num1, num2;
+    printf("\nEnter Two Numbers to Find LCM => ");
+    scanf("%d%d", &num1, &num2);
+
+    // Handing Wrong or Invalid Inputs
+    if (num1 * num2 <= 0)
+    {
+        printf("\nLCM of the Entered Number Are Not Possible !!! Both Numbers Must be Positive Integers...");
+        exit(0);
+    }
+
+    // 1st Approach
+    int max = num1 > num2 ? num1 : num2;
+    
+    for (int i = max; i <= num1 * num2; i += max)
+    {
+        if (i % num1 == 0 && i % num2 == 0) // We can also write if( !(i % num1) && !(i % num2) )
+        {
+            printf("\nLCM of %d and %d => %d", num1, num2, i);
+            break;
+        }
+    }
+
+    // 2nd Approach (Using Prime Factorisation Method)
+    // int lcm = 1;
+
+    // for (int i = 2; num1 * num2 != 1; i++)
+    // {
+    //     while (num1 % i == 0 || num2 % i == 0)
+    //     {
+    //         lcm = lcm * i;
+    //         num1 = num1 % i == 0 ? num1 / i : num1;
+    //         num2 = num2 % i == 0 ? num2 / i : num2;
+    //     }
+    // }
+
+    // printf("\nLCM => %d", lcm);
+
+    getch();
+    return 0;
+}
+// Main Function End
