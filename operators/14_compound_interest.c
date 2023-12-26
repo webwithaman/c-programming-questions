72388d6cffbdfd37f2f49db9f9ee72bcf0d583e6
aman <webwithaman06@gmail.com>
2023-12-26 17:19:31 +0530

    add: 14

---
+++ operators/14_compound_interest.c
@@ -0,0 +1,28 @@
+// C program calculate Compound Interest
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+// Main Function Start
+int main()
+{
+    float principal, rateOfInterest, time, compoundInterest, compoundedAfterMonths;
+    printf("\nEnter Pricipal Amount => ");
+    scanf("%f", &principal);
+    printf("\nEnter Rate of Interest => ");
+    scanf("%f", &rateOfInterest);
+    printf("\nEnter Time => ");
+    scanf("%f", &time);
+    printf("\nCompounded After Every N Months => ");
+    scanf("%f", &compoundedAfterMonths);
+
+    float n = 12 / compoundedAfterMonths;
+
+    compoundInterest = principal * pow((1 + rateOfInterest / (100 * n)), n * time) - principal;
+    printf("\nCompound Interest => %.2f", compoundInterest);
+
+    getch();
+    return 0;
+}
