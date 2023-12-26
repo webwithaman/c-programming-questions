af9e84c64b717d476f4ca10eaf69110955512bd8
aman <webwithaman06@gmail.com>
2023-12-26 11:59:14 +0530

    add: 09

---
+++ operators/10_area_perimeter.c
@@ -0,0 +1,20 @@
+// C program to find Area and Perimeter of a Rectangle
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+    float area, perimeter, l, b;
+    printf("\nEnter Lenght and Breadth of A Rectangle => ");
+    scanf("%f%f", &l, &b);
+    area = l * b;
+    perimeter = 2 * (l + b);
+    printf("\nArea of Rectangle => %.2f", area);
+    printf("\nPerimeter of Rectangle => %.2f", perimeter);
+
+    getch();
+    return 0;
+}
