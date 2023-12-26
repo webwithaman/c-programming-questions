eb50214655f2ed9a54f1afbed735b0a98125297f
aman <webwithaman06@gmail.com>
2023-12-26 10:49:59 +0530

    add: 04

---
+++ operators/03_ft_to_mtr.c
@@ -0,0 +1,22 @@
+// C program to convert Feets to Meters
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Define Macros
+#define FT_IN_1_MTR 3.280839
+
+// Main Function Start
+int main()
+{
+
+    float ft, mtr;
+    printf("\nEnter value in Feets to convert in Meters => ");
+    scanf("%f", &ft);
+    mtr = ft / FT_IN_1_MTR;
+    printf("\n%.4f Feet = %.4f Meter\n", ft, mtr);
+
+    getch();
+    return 0;
+}
---
+++ operators/04_mtr_to_ft.c
@@ -0,0 +1,22 @@
+// C program to convert Meters to Feets
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Define Macros
+#define FT_IN_1_MTR 3.280839
+
+// Main Function Start
+int main()
+{
+
+    float ft, mtr;
+    printf("\nEnter value in Meters to convert in Feets => ");
+    scanf("%f", &mtr);
+    ft = mtr * FT_IN_1_MTR;
+    printf("\n%.4f Meter = %.4f Feet\n", mtr, ft);
+
+    getch();
+    return 0;
+}
