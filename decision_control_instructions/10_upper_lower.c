9b360b87384eda330470cc3593a06e8a30050770
aman <webwithaman06@gmail.com>
2023-12-27 21:45:21 +0530

    add : decision_control 10

---
+++ decision_control_instructions/10_upper_lower.c
@@ -0,0 +1,24 @@
+// C program to input an alphabet from user and check whether the given alphabet is in uppercase or in lowercase
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+    char ch;
+    printf("\nEnter An Alphabet => ");
+    scanf("%c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z')
+        printf("\n%c is an Alphabet in Uppercase", ch);
+    else if (ch >= 'a' && ch <= 'z')
+        printf("\n%c is an Alphabet in Lowercase", ch);
+    else
+        printf("\nNot an Alphabet");
+
+    printf("\n");
+    getch();
+    return 0;
+}
