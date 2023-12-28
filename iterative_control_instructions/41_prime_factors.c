0f48fb7ae1c589162407b658d7110b6f37409c90
aman <webwithaman06@gmail.com>
2023-12-28 20:23:59 +0530

    add: iterative 41

---
+++ iterative_control_instructions/41_prime_factors.c
@@ -0,0 +1,16 @@
+// C program to find all Prime Factors of a given Number
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// Main Function Start
+int main()
+{
+
+    printf("\n");
+    getch();
+    return 0;
+}
