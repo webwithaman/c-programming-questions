448b18320ff5a1a2067a28e22054b414f75e8a32
aman <webwithaman06@gmail.com>
2023-12-28 13:10:38 +0530

    add: iterative 23

--- iterative_control_instructions/23_sum_of_first_n_odd_natural_nums.c
+++ iterative_control_instructions/23_sum_of_first_n_odd_natural_nums.c
@@ -1,4 +1,4 @@
-// C program to calculte sum of first N natural numbers
+// C program to calculte sum of first N odd natural numbers
 
 // Header Files
 #include <stdio.h>
@@ -10,7 +10,7 @@ int main()
 {
 
     int n, sum = 0;
-    printf("\nEnter N to Calculate Sum of First N Natural Numbers => ");
+    printf("\nEnter N to Calculate Sum of First N Odd Natural Numbers => ");
     scanf("%d", &n);
 
     if (n < 0)
@@ -21,24 +21,21 @@ int main()
 
     // // 1st Approach (using for loop)
     for (int i = 1; i <= n; i++)
-        sum += i;
+        sum += i * 2 - 1;
 
     // // 2nd Approach (using do-while loop)
     // int i = 1;
     // do
     // {
-    //   sum+=i++;
+    //     sum += i++ * 2 - 1;
     // } while (i <= n);
 
     // //  3rd Approach (using while loop)
     // int i = 1;
     // while (i <= n)
-    //     sum += i++;
+    //     sum += i++ * 2 - 1;
 
-    // // 4th Approach (using Direct formula)
-    // int sum = n * (n + 1) / 2;
-
-    printf("\nSum of First %d Natural Numbers => %d", n, sum);
+    printf("\nSum of First %d Odd Natural Numbers => %d", n, sum);
 
     printf("\n");
     getch();
