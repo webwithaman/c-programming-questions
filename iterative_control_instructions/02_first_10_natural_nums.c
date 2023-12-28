06d7ed1dbc58c9473248b3f0da8fca69265f60b2
aman <webwithaman06@gmail.com>
2023-12-28 11:28:48 +0530

    add: iterative 02

---
+++ iterative_control_instructions/02_first_10_natural_nums.c
@@ -0,0 +1,48 @@
+// C program to print the first 10 natural numbers
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+    printf("\n>>>>>>>>> First 10 Natural Numbers <<<<<<<<<\n");
+
+    // //  1st Approach (using while loop)
+    // int i = 1;
+    // while (i <= 10)
+    // {
+    //     printf("\n%d", i);
+    //     i++;
+    // }
+
+    // // 2nd Approach (using while loop)
+    // int i = 0;
+    // while (i++ < 10)
+    //     printf("\n%d", i);
+
+    // // 3rd Approach (using do-while loop)
+    // int i = 1;
+    // do
+    // {
+    //     printf("\n%d", i);
+    //     i++;
+    // } while (i <= 10);
+
+    // // 4th Approach (using do-while loop)
+    // int i = 0;
+    // do
+    // {
+    //     printf("\n%d", i + 1);
+    // } while (++i < 10);
+
+    // // 5th Approach (using for loop)
+    // for (int i = 1; i <= 10; i++)
+    //     printf("\n%d", i);
+
+    printf("\n");
+    getch();
+    return 0;
+}
