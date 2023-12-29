d1abdf49f4983ca08a66b9d732f0e171799768fa
aman <webwithaman06@gmail.com>
2023-12-29 23:08:18 +0530

    add: Patterns 32

---
+++ patterns_problems/32_pattern.c
@@ -0,0 +1,74 @@
+// C proram to print following Pattern
+
+/*
+
+Pattern 32.
+
+    *
+    **
+    ***
+    ****
+    *****
+    ****
+    ***
+    **
+    *
+
+*/
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// Main Function Start
+int main()
+{
+    int maxRows, maxCols;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &maxRows);
+
+    // Handling Invalid Input
+    if (maxRows < 1)
+    {
+        printf("\n!!! Invalid Input,Plz Enter Positive Number....");
+        exit(0);
+    }
+
+    // Determine Number of Columns According to maxRows
+    maxCols = maxRows;
+
+    // // Print Pattern
+    puts("\n--------------------------------------------\n");
+
+    // // 1st Approach
+    int colsInCurrentRow;
+
+    for (int row = 1; row <= maxRows; row++)
+    {
+        colsInCurrentRow = row;
+
+        for (int col = 1; col <= colsInCurrentRow; col++)
+            printf("%d ", col);
+
+        printf("\n");
+    }
+
+    // // 2nd Approach
+    // for (int row = 1; row <= maxRows; row++)
+    // {
+    //     for (int col = 1; col <= maxCols; col++)
+    //     {
+    //         if (col <= row)
+    //             printf("%d ", col);
+    //         else
+    //             printf("  ");
+    //     }
+    //     printf("\n");
+    // }
+
+    printf("\n");
+    getch();
+    return 0;
+}
+// Main Function End
