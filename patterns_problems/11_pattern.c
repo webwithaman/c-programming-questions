61d68b891919ea3176d9317be186fb198a361c4f
aman <webwithaman06@gmail.com>
2023-12-29 16:23:31 +0530

    add: Patterns 11

---
+++ patterns_problems/11_pattern.c
@@ -0,0 +1,101 @@
+// C proram to print following Pattern
+
+/*
+
+Pattern 11.
+
+        A
+       ABA
+      ABCBA
+     ABCDCBA
+    ABCDEDCBA
+
+*/
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// Main Function Start
+int main()
+{
+    int maxRows, maxCols;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &maxRows);
+
+    // Handling Invalid Input
+    if (maxRows < 1)
+    {
+        printf("\n!!! Invalid Input,Plz Enter Positive Number....");
+        exit(0);
+    }
+
+    // Determine Number of Columns According to maxRows
+    maxCols = maxRows * 2 - 1;
+
+    // // Print Pattern
+    puts("\n--------------------------------------------\n");
+
+    // // 1st Approach
+    int spacesInCurrentRow;
+
+    for (int row = 1; row <= maxRows; row++)
+    {
+        spacesInCurrentRow = (maxRows - row) * 2;
+
+        for (int space = 1; space <= spacesInCurrentRow / 2; space++)
+            printf(" ");
+
+        for (int col = 1; col <= row; col++)
+            printf("%c", 'A' - 1 + col);
+
+        for (int col = row - 1; col; col--)
+            printf("%c", 'A' - 1 + col);
+
+        printf("\n");
+    }
+
+    // // 2nd Approach
+    // int spacesInCurrentRow, charAtCol;
+
+    // for (int row = 1; row <= maxRows; row++)
+    // {
+    //     spacesInCurrentRow = (maxRows - row) * 2;
+    //     charAtCol = 'A';
+
+    //     for (int space = 1; space <= spacesInCurrentRow / 2; space++)
+    //         printf(" ");
+
+    //     for (int col = 1; col <= row; col++)
+    //         printf("%c", charAtCol++);
+
+    //     charAtCol -= 2;
+    //     for (int col = row - 1; col; col--)
+    //         printf("%c", charAtCol--);
+
+    //     printf("\n");
+    // }
+
+    // // 3rd Approach
+    // int charAtCol;
+
+    // for (int row = 1; row <= maxRows; row++)
+    // {
+    //     charAtCol = 'A';
+
+    //     for (int col = 1; col <= maxCols; col++)
+    //     {
+    //         if (col >= maxRows + 1 - row && col <= maxRows - 1 + row)
+    //             printf("%c", col < maxRows ? charAtCol++ : charAtCol--);
+    //         else
+    //             printf(" ");
+    //     }
+
+    //     printf("\n");
+    // }
+
+    printf("\n");
+    getch();
+    return 0;
+}
