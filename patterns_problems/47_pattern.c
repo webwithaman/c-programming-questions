1233dfaf73bf5b27e3aa499e7dc915d42f8369d1
aman <webwithaman06@gmail.com>
2023-12-30 20:56:35 +0530

    add: pattern 47 create

--- patterns_problems/47_pattern.c
+++ patterns_problems/47_pattern.c
@@ -16,6 +16,9 @@ Pattern 47.
 #include <conio.h>
 #include <stdlib.h>
 
+// Function Prototype (Declaration)
+int sumOfFirstNNums(int);
+
 // Main Function Start
 int main()
 {
@@ -31,16 +34,94 @@ int main()
     }
 
     // Determine Number of Columns According to maxRows
-    maxCols = maxRows + 1;
+    maxCols = maxRows * 2;
 
     // // Print Pattern
     puts("\n--------------------------------------------\n");
 
     // // 1st Approach
-   
+    int sum = sumOfFirstNNums(maxRows), addNum;
+
+    for (int row = 1; row <= maxRows; row++)
+    {
+        for (int space = 1; space <= row - 1; space++)
+            printf("    ");
+
+        addNum = sum - sumOfFirstNNums(maxRows + 1 - row);
+
+        for (int col = 1; col <= maxRows + 1 - row; col++)
+            printf("%3d ", col + addNum);
+
+        addNum = sum + sumOfFirstNNums(maxRows - row);
+
+        for (int col = 1; col <= maxRows + 1 - row; col++)
+            printf("%3d ", col + addNum);
+
+        printf("\n");
+    }
+
+    // // 2nd Approach
+    // int sum = sumOfFirstNNums(maxRows), countForLeft = 1, countForRight;
+
+    // for (int row = 1; row <= maxRows; row++)
+    // {
+    //     countForRight = sum + 1 + sumOfFirstNNums(maxRows - row);
+
+    //     for (int col = 1; col <= maxCols; col++)
+    //     {
+    //         if (col >= row && col <= maxCols + 1 - row)
+    //         {
+    //             if (col <= maxRows)
+    //                 printf("%3d ", countForLeft++);
+    //             else
+    //                 printf("%3d ", countForRight++);
+    //         }
+    //         else
+    //             printf("    ");
+    //     }
+
+    //     printf("\n");
+    // }
+
+    // // 3rd Approach
+    // int sum = maxRows * (maxRows + 1) / 2, numAtCol, lock;
+
+    // for (int row = 1; row <= maxRows; row++)
+    // {
+    //     numAtCol = 1 + sum - sumOfFirstNNums(maxRows + 1 - row);
+    //     lock = 1;
+
+    //     for (int col = 0; col <= maxCols; col++)
+    //     {
+    //         if (col >= row && col <= maxCols + 1 - row)
+    //         {
+    //             if (col <= maxRows)
+    //                 printf("%3d ", numAtCol++);
+    //             else
+    //             {
+    //                 if (lock)
+    //                 {
+    //                     numAtCol += sumOfFirstNNums(maxRows - row) * 2;
+    //                     lock = 0;
+    //                 }
+    //                 printf("%3d ", numAtCol++);
+    //             }
+    //         }
+    //         else
+    //             printf("    ");
+    //     }
+
+    //     printf("\n");
+    // }
 
     printf("\n");
     getch();
     return 0;
 }
 // Main Function End
+
+// Function to Find Sum of First N Natural Numbers
+int sumOfFirstNNums(int n)
+{
+    return n * (n + 1) / 2;
+}
