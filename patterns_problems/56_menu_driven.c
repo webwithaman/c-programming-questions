e9db5f66b93944e6a673eb3281e5df79f851bb0d
aman <webwithaman06@gmail.com>
2023-12-31 22:24:58 +0530

    add : pattern 56 function pattern_46

--- patterns_problems/56_menu_driven.c
+++ patterns_problems/56_menu_driven.c
@@ -3682,3 +3682,66 @@ void pattern_45()
         printf("\n");
     }
 }
+
+void pattern_46()
+{
+    /*
+
+    Pattern 41.
+
+              *****
+             *   *
+            *   *
+           *   *
+          *****
+
+    */
+
+    int maxRows, maxCols;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &maxRows);
+
+    // Handling Invalid Input
+    if (maxRows < 1)
+    {
+        printf("\n!!! Invalid Input,Plz Enter Positive Number....");
+        exit(0);
+    }
+
+    // Determine Number of Columns According to maxRows
+    maxCols = maxRows * 2 - 1;
+
+    // // Print Pattern
+    puts("\n--------------------------------------------\n");
+
+    // // 1st Approach
+    for (int row = 1; row <= maxRows; row++)
+    {
+        for (int space = 1; space <= maxRows - row; space++)
+            printf(" ");
+
+        for (int col = 1; col <= maxRows; col++)
+        {
+            if (col == 1 || col == maxRows || row == 1 || row == maxRows)
+                printf("*");
+            else
+                printf(" ");
+        }
+
+        printf("\n");
+    }
+
+    // // 2nd Approach
+    // for (int row = 1; row <= maxRows; row++)
+    // {
+    //     for (int col = 1; col <= maxCols; col++)
+    //     {
+    //         if (col == maxRows + 1 - row || col == maxCols + 1 - row || row == 1 && col >= maxRows + 1 - row || row == maxRows && col <= maxCols + 1 - row)
+    //             printf("*");
+    //         else
+    //             printf(" ");
+    //     }
+
+    //     printf("\n");
+    // }
+}
