f9fa566b272edef38c901180a33ddec381db62ff
aman <webwithaman06@gmail.com>
2023-12-31 22:07:10 +0530

    add : pattern 56 function pattern_40

--- patterns_problems/56_menu_driven.c
+++ patterns_problems/56_menu_driven.c
@@ -3312,3 +3312,87 @@ void pattern_39()
     //     printf("\n");
     // }
 }
+
+void pattern_40()
+{
+    /*
+
+    Pattern 40.
+
+           *        *
+           **      **
+           ***    ***
+           ****  ****
+           **********
+           ****  ****
+           ***    ***
+           **      **
+           *        *
+
+    */
+
+    int maxRows, maxCols;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &maxRows);
+
+    // Handling Invalid Input
+    if (maxRows < 1)
+    {
+        printf("\n!!! Invalid Input,Plz Enter Positive Number....");
+        exit(0);
+    }
+
+    // Determine Number of Columns According to maxRows
+    maxCols = maxRows + 1;
+
+    // // Print Pattern
+    puts("\n--------------------------------------------\n");
+
+    // // 1st Approach
+    int vary = 1;
+
+    for (int row = 1; row <= maxRows; row++)
+    {
+        for (int col = 1; col <= maxCols; col++)
+        {
+            if (col <= vary || col >= maxCols + 1 - vary)
+                printf("*");
+            else
+                printf(" ");
+        }
+
+        row < maxCols / 2 ? vary++ : vary--;
+
+        // if user wants even number of rows
+        if (maxRows % 2 == 0 && row == maxCols / 2)
+            vary++;
+
+        printf("\n");
+    }
+
+    // // 2nd Approach
+    // int spacesInCurrentRow;
+    // int vary = 1;
+
+    // for (int row = 1; row <= maxRows; row++)
+    // {
+    //     spacesInCurrentRow = maxCols - vary * 2;
+
+    //     for (int col = 1; col <= vary; col++)
+    //         printf("*");
+
+    //     for (int space = 1; space <= spacesInCurrentRow; space++)
+    //         printf(" ");
+
+    //     for (int col = 1; col <= vary; col++)
+    //         printf("*");
+
+    //     row < maxCols / 2 ? vary++ : vary--;
+
+    //     // if user wants even number of rows
+    //     if (maxRows % 2 == 0 && row == maxCols / 2)
+    //         vary++;
+
+    //     printf("\n");
+    // }
+}
