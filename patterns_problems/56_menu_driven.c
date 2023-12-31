71fa9d9033c744fbea24ac038406aac194e2aa3a
aman <webwithaman06@gmail.com>
2023-12-31 20:50:32 +0530

    add : pattern 56 pattern_5 fun

--- patterns_problems/56_menu_driven.c
+++ patterns_problems/56_menu_driven.c
@@ -566,3 +566,84 @@ void pattern_4()
     // }
 }
 
+void pattern_5()
+{
+    /*
+
+    Pattern 05.
+
+            *
+           ***
+          *****
+         *******
+        *********
+
+    */
+
+    int maxRows, maxCols;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &maxRows);
+
+    // Handling Invalid Input
+    if (maxRows < 1)
+    {
+        printf("\n!!! Invalid Input,Plz Enter Positive Number....");
+        exit(0);
+    }
+
+    // Determine Number of Columns According to maxRows
+    maxCols = maxRows * 2 - 1;
+
+    // // Print Pattern
+    puts("\n--------------------------------------------\n");
+
+    // // 1st Approach
+    int colsInCurrentRow, spacesInCurrentRow;
+
+    for (int row = 1; row <= maxRows; row++)
+    {
+        spacesInCurrentRow = (maxRows - row) * 2;
+        colsInCurrentRow = maxCols - spacesInCurrentRow;
+        // colsInCurrentRow = row * 2 - 1;
+
+        for (int space = 1; space <= spacesInCurrentRow / 2; space++)
+            printf(" ");
+
+        for (int col = 1; col <= colsInCurrentRow; col++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    // // 2nd Approach
+    // int spacesInCurrentRow, colsInCurrentRow = -1;
+
+    // for (int row = 1; row <= maxRows; row++)
+    // {
+    //     spacesInCurrentRow = (maxRows - row) * 2;
+
+    //     colsInCurrentRow += 2;
+
+    //     for (int space = 1; space <= spacesInCurrentRow / 2; space++)
+    //         printf(" ");
+
+    //     for (int col = 1; col <= colsInCurrentRow; col++)
+    //         printf("*");
+
+    //     printf("\n");
+    // }
+
+    // // 3rd Approach
+    // for (int row = 1; row <= maxRows; row++)
+    // {
+    //     for (int col = 1; col <= maxCols; col++)
+    //     {
+    //         if (col >= maxRows + 1 - row && col <= maxRows - 1 + row)
+    //             printf("*");
+    //         else
+    //             printf(" ");
+    //     }
+
+    //     printf("\n");
+    // }
+}
