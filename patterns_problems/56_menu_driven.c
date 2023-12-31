a462b50e91ce722442899fdb459e7a986ab91701
aman <webwithaman06@gmail.com>
2023-12-31 21:39:38 +0530

    add : pattern 56 pattern22 fun

--- patterns_problems/56_menu_driven.c
+++ patterns_problems/56_menu_driven.c
@@ -1889,6 +1889,55 @@ void pattern_21()
     // }
 }
 
-void pattern_22(){
-    
+void pattern_22()
+{
+    /*
+
+    Pattern 22.
+
+        A B C D
+        A B C D
+        A B C D
+        A B C D
+
+    */
+
+    int maxRows, maxCols;
+    printf("\nHow Many Rows => ");
+    scanf("%d", &maxRows);
+
+    // Handling Invalid Input
+    if (maxRows < 1)
+    {
+        printf("\n!!! Invalid Input,Plz Enter Positive Number....");
+        exit(0);
+    }
+
+    // Determine Number of Columns According to maxRows
+    maxCols = maxRows;
+
+    // // Print Pattern
+    puts("\n--------------------------------------------\n");
+
+    // // 1st Approach
+    for (int row = 1; row <= maxRows; row++)
+    {
+
+        for (int col = 1; col <= maxCols; col++)
+            printf("%c ", 'A' - 1 + col);
+
+        printf("\n");
+    }
+
+    // // 2nd Approach
+    // char charAtCol;
+
+    // for (int row = 1; row <= maxRows; row++)
+    // {
+    //     charAtCol = 'A';
+    //     for (int col = 1; col <= maxCols; col++)
+    //         printf("%c ", charAtCol++);
+
+    //     printf("\n");
+    // }
 }
