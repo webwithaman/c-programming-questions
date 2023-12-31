ed36849af9dc8f8f4ec4259ec8509febcf80b6ea
aman <webwithaman06@gmail.com>
2023-12-31 22:37:44 +0530

    add : switch case 01

---
+++ switch_case_control_instructions/01_days_in_month.c
@@ -0,0 +1,15 @@
+// C program which takes the month number as an input and display number of days in that month.
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+    printf("\n");
+    getch();
+    return 0;
+}
+// Main Function End
