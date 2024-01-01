cc4642545678885c2b70126e92aa52f752eea8a7
aman <webwithaman06@gmail.com>
2024-01-01 23:58:59 +0530

    add : switch-case 02

---
+++ switch_case_control_instructions/03_greeting.c
@@ -0,0 +1,44 @@
+// WAP which takes the day number of a week and displays a unique greeting message for the day.
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+
+       int dayNumber;
+       printf("\nEnter Any Day Number of the Week => ");
+
+    
+        switch (dayN)
+        {
+        case 'a':
+            printf("\n%f + %f => %f", num1, num2, num1 + num2);
+            break;
+        case 'b':
+            printf("\n%f - %f => %f", num1, num2, num1 - num2);
+            break;
+        case 'c':
+            printf("\n%f x %f => %f", num1, num2, num1 * num2);
+            break;
+        case 'd':
+            printf("\n%f / %f => %f", num1, num2, num1 / num2);
+            break;
+        case 'e':
+            printf("\nProgram End...");
+            exit(0);
+            break;
+        default:
+            puts("\n!!! Invalid Choice....");
+        }
+
+        getch();
+    }
+
+    printf("\n");
+    getch();
+    return 0;
+}
+// Main Function End
