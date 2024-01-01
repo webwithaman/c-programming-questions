0964b6c9ac4577c207c7cb6deefd927160abdb2c
aman <webwithaman06@gmail.com>
2024-01-01 21:05:34 +0530

    add : switch-case 01

--- switch_case_control_instructions/01_days_in_month.c
+++ switch_case_control_instructions/01_days_in_month.c
@@ -3,10 +3,48 @@
 // Header Files
 #include <stdio.h>
 #include <conio.h>
+#include <time.h>
 
 // Main Function Start
 int main()
 {
+    int monthNumber;
+    printf("\nEnter Any Month Number to Know Number of Days In That Month => ");
+    scanf("%d", &monthNumber);
+
+    switch (monthNumber)
+    {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        printf("\nThere Are 31 Days In Entered Month");
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        printf("\nThere Are 31 Days In Entered Month");
+        break;
+    case 2:
+    {
+        time_t s;
+        struct tm *current_time;
+        s = time(NULL);
+        current_time = localtime(&s);
+        int currentYear = current_time->tm_year + 1900;
+        if (currentYear % 400 == 0 || (currentYear % 100 && currentYear % 4 == 0))
+            printf("\nThere Are 29 Days In Entered Month");
+        else
+            printf("\nThere Are 28 Days In Entered Month");
+    }
+    break;
+    default:
+        printf("\n!!! Invalid Month Number...");
+    }
 
     printf("\n");
     getch();
