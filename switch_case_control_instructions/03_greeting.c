0bff6ea098a836153c60f4b141144ed321db8007
aman <webwithaman06@gmail.com>
2024-01-02 00:00:04 +0530

    add : switch-case 04

--- switch_case_control_instructions/03_greeting.c
+++ switch_case_control_instructions/03_greeting.c
@@ -18,8 +18,7 @@ int main()
             printf("\n%f + %f => %f", num1, num2, num1 + num2);
             break;
         case 'b':
-            printf("f => %f", num1, num2, num1 - num2);
-        
+            printf("f => %f", num1, num2, num1 - n
         case 'c':
             printf("\n%f x %f => %f", num1, num2, num1 * num2);
             break;
