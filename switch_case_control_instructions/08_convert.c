817c14438405a6a575ef76f3491c1559d630ba1f
aman <webwithaman06@gmail.com>
2024-01-02 15:32:56 +0530

    add : switch-case 08

--- switch_case_control_instructions/08_convert.c
+++ switch_case_control_instructions/08_convert.c
@@ -1,6 +1,5 @@
 // Program to convert a positive number into a negative number and negative number into a positive number using a switch statement
 
-
 // Header Files
 #include <stdio.h>
 #include <conio.h>
@@ -8,19 +7,18 @@
 // Main Function Start
 int main()
 {
+    int num, result;
+    printf("\nEnter A Number => ");
+    scanf("%d", &num);
 
-    int year;
-    printf("\nEnter Any Year to Check Whether It is Leap Year or Not => ");
-    scanf("%d", &year);
-
-    switch ((year % 400 == 0) || (year % 100 && year % 4 == 0))
+    switch (num != 0)
     {
-    case 0:
-        printf("\n%d is Not A Leap Year", year);
-        break;
     case 1:
-        printf("\n%d is Leap Year", year);
+        result = -num;
+        printf("\n%d Convert Into %d", num, result);
         break;
+    default:
+        printf("\nEntered Number is Zero");
     }
 
     printf("\n");
