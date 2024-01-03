c1df64de1f8f2b0451bdf139b280fe23b55536a3
aman <webwithaman06@gmail.com>
2024-01-03 14:49:18 +0530

    add : functions 04

---
+++ functions/04_first_n_natural.c
@@ -0,0 +1,32 @@
+// Write a function to print first N natural numbers (TSRN)
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Function Declaration (Prototype)
+void firstNNaturalNums(int);
+
+// Main Function Start
+int main()
+{
+    int n;
+    printf("\nEnter N to Print First N Natural Numbers => ");
+    scanf("%d", &n);
+    firstNNaturalNums(n);
+
+    getch();
+    return 0;
+}
+// Main Function End
+
+// Function Definition
+void firstNNaturalNums(int n)
+{
+    putch(10); // It means putch('\n') , ASCII of '\n' is 10
+
+    for (int i = 1; i <= n; i++)
+        printf("%d ", i);
+
+    putch('\n');
+}
