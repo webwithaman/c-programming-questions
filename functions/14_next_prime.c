194c043a2ce0fee0caaa65590f16733106eb5cf0
aman <webwithaman06@gmail.com>
2024-01-05 00:59:36 +0530

    modify : functions 14

---
+++ functions/14_next_prime.c
@@ -0,0 +1,51 @@
+// Write a function to find the next prime number of a given number. (TSRS)
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+// Function Declaration (Prototype)
+int nextPrime(int);
+int isPrime(int);
+
+// Main Function Start
+int main()
+{
+    int num;
+    printf("\nEnter A Number to Find Next Prime Number of it => ");
+    scanf("%d", &num);
+
+    printf("\nNext Prime Number of %d => %d", num, nextPrime(num));
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
+
+// Function Definitions 👇👇
+
+// Function to Find Next Prime Number of A Given Number
+int nextPrime(int num)
+{
+    while (isPrime(++num) != 1)
+        ;
+
+    return num;
+}
+
+// Function to Check Whether A Number is Prime or Not
+int isPrime(int num)
+{
+    if (num < 2)
+        return -1; // Invalid Input
+
+    for (int i = 2; i <= sqrt(num); i++)
+    {
+        if (num % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
