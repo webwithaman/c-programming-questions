be2f15651483dd73e91ff7031c8b932b9acad77a
aman <webwithaman06@gmail.com>
2024-01-05 17:29:22 +0530

    add : recursion 07

---
+++ recursion/07_squares_of_n.c
@@ -0,0 +1,43 @@
+// Write a recursive function to print squares of first N natural numbers.
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// Function Declaration (Prototype)
+void squaresOfFirstN(int);
+
+// Main Function Start
+int main()
+{
+    int n;
+    printf("\nEnter N to Print Squares of First N Natural Numbers => ");
+    scanf("%d", &n);
+
+    if (n < 0)
+    {
+        puts("\n!!! Invalid Input, Plz Enter Positive Number...");
+        exit(0);
+    }
+
+    printf("\n>>>>>>> Squares of First %d Natural Numbers <<<<<<<\n", n);
+    squaresOfFirstN(n);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
+
+// Function Definition 👇👇
+
+// Recursive Function to Print Squares of First N Natural Numbers
+void squaresOfFirstN(int n)
+{
+    if (n > 0)
+    {
+        squaresOfFirstN(n - 1);
+        printf("%d ", n * n);
+    }
+}
