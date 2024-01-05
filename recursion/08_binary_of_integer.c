49e6b73b47cd55fb903bb103c6965802d5899604
aman <webwithaman06@gmail.com>
2024-01-05 18:41:47 +0530

    add : recursion 08

---
+++ recursion/08_binary_of_integer.c
@@ -0,0 +1,43 @@
+// Write a recursive function to print binary of a given Positive Integer
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// Function Declaration (Prototype)
+void binaryOfPositiveInteger(int);
+
+// Main Function Start
+int main()
+{
+    int num;
+    printf("\nEnter A Positive Number to Find its Binary => ");
+    scanf("%d", &num);
+
+    if (num < 0)
+    {
+        puts("\n!!! Invalid Input, Plz Enter Positive Number...");
+        exit(0);
+    }
+
+    printf("\n>>>>>>> Binary of %d <<<<<<<\n", num);
+    binaryOfPositiveInteger(num);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
+
+// Function Definition 👇👇
+
+// Recursive Function to Print Binary of A Positive Integer
+void binaryOfPositiveInteger(int num)
+{
+    if (num > 0)
+    {
+        binaryOfPositiveInteger(num / 2);
+        printf("%d ", num % 2);
+    }
+}
