58ecb0cc103d5511cbea705199cdae7c6d930da7
aman <webwithaman06@gmail.com>
2024-01-06 14:27:42 +0530

    add : arrays with functions 01

---
+++ arrays_with_functions/01_largest.c
@@ -0,0 +1,54 @@
+// Write a function to find the largest number from the given array of any size. (TSRS)
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// Functions Declarations (Prototypes)
+int largest(int[], int);
+
+// Main Function Start
+int main()
+{
+    const int ARRAY_SIZE;
+    printf("\nHow Many Numbers You Want to Enter => ");
+    scanf("%d", &ARRAY_SIZE);
+
+    if (ARRAY_SIZE < 1)
+    {
+        puts("\n!!! Invalid Input, Plz Correctly Specify Number of Elements...");
+        exit(0);
+    }
+
+    // Declare Array of Variable size
+    int nums[ARRAY_SIZE];
+
+    // Input Elements
+    printf("\nEnter %d Numbers => ", ARRAY_SIZE);
+    for (int i = 0; i < ARRAY_SIZE; i++)
+        scanf("%d", &nums[i]);
+
+    printf("\nLargest Number => %d", largest(nums, ARRAY_SIZE));
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
+
+// Functions Definitions 👇👇
+
+// Function to Find Largest Element of An Array
+int largest(int arr[], int size)
+{
+    int largest = arr[0];
+
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > largest)
+            largest = arr[i];
+    }
+
+    return largest;
+}
