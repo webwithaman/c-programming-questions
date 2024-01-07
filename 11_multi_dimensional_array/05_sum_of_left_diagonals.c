d72c3c1a80c4ab201118262027b25b117c14986b
aman <webwithaman06@gmail.com>
2024-01-07 12:15:43 +0530

    add : multi-dim 05

---
+++ 11_multi_dimensional_array/05_sum_of_left_diagonals.c
@@ -0,0 +1,67 @@
+//  Write a program in C to find the sum of left diagonals of a matrix.
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define MAX_ROWS 10
+#define MAX_COLS 10
+
+// Main Function Start
+int main()
+{
+    const int ROWS, COLS;
+    printf("\nEnter Order of Square Matrix-A (Rows x Cols) (MAX %d x %d) => ", MAX_ROWS, MAX_COLS);
+    scanf("%d%d", &ROWS, &COLS);
+
+    // Check Invalid Input for Matrix Order
+    if (ROWS < 1 || ROWS != COLS || ROWS > MAX_ROWS || COLS < 1 || COLS > MAX_COLS)
+    {
+        puts("\n!!! Invalid order of Matrix, Plz Enter Appropriate Order...\n");
+        exit(0);
+    }
+
+    // Declare 2-d Array of Entered Order
+    int matrixA[ROWS][COLS], sumOfLeftDiagonals = 0;
+
+    // Input Elements of Matrix-A
+    printf("\n>>>>>> Enter Elements of Matrix-A of Order %d x %d <<<<<<<\n", ROWS, COLS);
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            printf("\nEnter element[%d][%d] => ", i + 1, j + 1);
+            scanf("%d", &matrixA[i][j]);
+        }
+    }
+
+    // Print Matrix-A
+    printf("\n\n>>>>>>>> Matrix-A of %d x %d <<<<<<<<<\n", ROWS, COLS);
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+            printf("%4d ", matrixA[i][j]);
+
+        putch(10); // Add New line
+    }
+
+    // Find Sum of Left Diagonals
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            if (i == j)
+                sumOfLeftDiagonals += matrixA[i][j];
+        }
+    }
+
+    // Print Sum of Left Diagonals
+    printf("\nSum of Left Diagonals => %d", sumOfLeftDiagonals);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
