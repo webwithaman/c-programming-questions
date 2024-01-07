9c67966f50a0bd58fb67676329fc2bc07bd925e9
aman <webwithaman06@gmail.com>
2024-01-07 09:51:14 +0530

    add : multi-dim 01

--- 11_multi_dimensional_array/01_sum_of_matrices.c
+++ 11_multi_dimensional_array/01_sum_of_matrices.c
@@ -1,2 +1,29 @@
 // Write a program to calculate the sum of two matrices each of order 3x3.
 
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+#define ARRAY_SIZE 10
+
+// Main Function Start
+int main()
+{
+    int nums[ARRAY_SIZE], sum = 0;
+
+    printf("\nEnter 10 Numbers to Calculate Sum => ");
+
+    // Input Elements and Add
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        scanf("%d", &nums[i]);
+        sum += nums[i];
+    }
+
+    printf("\nSum of Entered Numbers => %d", sum);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
