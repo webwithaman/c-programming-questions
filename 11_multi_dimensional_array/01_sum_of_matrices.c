3c200490b7759e15e96d81b80b8ce05ccb49b766
aman <webwithaman06@gmail.com>
2024-01-07 10:10:57 +0530

    add : multi-dim 01

--- 11_multi_dimensional_array/01_sum_of_matrices.c
+++ 11_multi_dimensional_array/01_sum_of_matrices.c
@@ -4,23 +4,71 @@
 #include <stdio.h>
 #include <conio.h>
 
-#define ARRAY_SIZE 10
+#define ROWS 3
+#define COLS 3
 
 // Main Function Start
 int main()
 {
-    int nums[ARRAY_SIZE], sum = 0;
+    // Declare 2-d Arrays
+    int matrix1[ROWS][COLS], matrix2[ROWS][COLS], sumOfMatrices[ROWS][COLS];
 
-    printf("\nEnter 10 Numbers to Calculate Sum => ");
+    // Input Elements of 1st Matrix
+    printf("\n>>>>>> Enter Elements of 1st Matrix of Order %d x %d <<<<<<<\n", ROWS, COLS);
 
-    // Input Elements and Add
-    for (int i = 0; i < ARRAY_SIZE; i++)
+    for (int i = 0; i < ROWS; i++)
     {
-        scanf("%d", &nums[i]);
-        sum += nums[i];
+        printf("\nEnter %d Elements For Row %d => ", COLS, i + 1);
+        for (int j = 0; j < COLS; j++)
+            scanf("%d", &matrix1[i][j]);
     }
 
-    printf("\nSum of Entered Numbers => %d", sum);
+    // Input Elements of 2nd Matrix
+    printf("\n\n>>>>>> Enter Elements of 2nd Matrix of Order %d x %d <<<<<<<\n", ROWS, COLS);
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        printf("\nEnter %d Elements For Row %d => ", COLS, i + 1);
+        for (int j = 0; j < COLS; j++)
+            scanf("%d", &matrix2[i][j]);
+    }
+
+    // Add Matrices
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+            sumOfMatrices[i][j] = matrix1[i][j] + matrix2[i][j];
+    }
+
+    // Print Matrix-1
+    printf("\n\n>>>>>>>> 1st Matrix of %d x %d <<<<<<<<<\n", ROWS, COLS);
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+            printf("%4d ", matrix1[i][j]);
+
+        putch(10); // Add New line
+    }
+
+    // Print Matrix-2
+    printf("\n\n>>>>>>>> 2nd Matrix of %d x %d <<<<<<<<<\n", ROWS, COLS);
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+            printf("%4d ", matrix2[i][j]);
+
+        putch(10); // Add New line
+    }
+
+    // Print Resultant (sumOfMatrices)
+    printf("\n\n>>>>>>>> Resultant Matrix of %d x %d <<<<<<<<<\n", ROWS, COLS);
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+            printf("%4d ", sumOfMatrices[i][j]);
+
+        putch(10); // Add New line
+    }
 
     putch('\n');
     getch();
