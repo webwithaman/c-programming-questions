d9697b30d22930751907e9771175cc4a6f3f1f56
aman <webwithaman06@gmail.com>
2024-01-09 20:58:49 +0530

    add : strings 08

---
+++ 13_strings/08_copy_string.c
@@ -0,0 +1,33 @@
+// Write a program in C to copy one string to another string.
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+#define ARRAY_SIZE 31
+
+// Main Function Start
+int main()
+{
+    char str[ARRAY_SIZE], copyStr[ARRAY_SIZE];
+
+    printf("\nEnter Any String to Copy Make Its Copy (MAX CHARACTERS %d) => ", ARRAY_SIZE - 1);
+    fgets(str, ARRAY_SIZE, stdin);  // Input String
+    str[strcspn(str, "\n")] = '\0'; // Replace '\n' character with '\0' in str
+
+    // Copy str into copyStr
+    int i = 0;
+    for (i = 0; str[i]; i++)
+        copyStr[i] = str[i];
+
+    copyStr[i] = '\0'; // Add Null Character at End
+
+    printf("\nOriginal String => %s", str);
+    printf("\nCopy of Entered String => %s", copyStr);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
