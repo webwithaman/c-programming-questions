d170624f2a52f3c0648f9d3e1fcb928859e7cead
aman <webwithaman06@gmail.com>
2024-01-09 20:21:06 +0530

    add : strings 03

---
+++ 13_strings/03_count_vowels.c
@@ -0,0 +1,39 @@
+// Write a program to count vowels in a given string.
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+#define ARRAY_SIZE 31
+
+// Main Function Start
+int main()
+{
+    char str[ARRAY_SIZE], ch, count = 0, vowels[11] = "AEIOUaeiou";
+
+    printf("\nEnter Any String to Count Vowels in it (MAX CHARACTERS %d) => ", ARRAY_SIZE - 1);
+    fgets(str, ARRAY_SIZE, stdin);  // Input String
+    str[strcspn(str, "\n")] = '\0'; // Replace '\n' character with '\0' in str
+
+    // Find Occurrence of ch in str
+    for (int i = 0; str[i]; i++)
+    {
+        for (int j = 0; vowels[j]; j++)
+        {
+            if (str[i] == vowels[j])
+            {
+                count++;
+                break;
+            }
+        }
+    }
+
+    // Print Occurrence
+    printf("\nThere are %d vowels in \"%s\" ", count, str);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
