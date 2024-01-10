c174c2fd82d64f32a40ea6a0b9c836bac7a21f77
aman <webwithaman06@gmail.com>
2024-01-10 11:41:26 +0530

    add : string with functios 02

--- 14_string_with_functions/02_str_len_pre_def.c
+++ 14_string_with_functions/02_str_len_pre_def.c
@@ -7,9 +7,6 @@
 
 #define ARRAY_SIZE 31
 
-// Function Declarations
-int strLength(char[]);
-
 // Main Function Start
 int main()
 {
@@ -19,7 +16,7 @@ int main()
     fgets(name, ARRAY_SIZE, stdin);   // Input String
     name[strcspn(name, "\n")] = '\0'; // Replace '\n' character with '\0' in name
 
-    length = strLength(name); // Find Length using user-defined function
+    length = strlen(name); // Find Length using pre-defined function
 
     printf("\nYour Name is \"%s\" and Number of Characters in Your Name => %d", name, length);
 
@@ -28,16 +25,3 @@ int main()
     return 0;
 }
 // Main Function End
-
-// Function Definitions 👇👇
-
-// Function to Calculate Length of String
-int strLength(char str[])
-{
-    int length = 0;
-
-    while (str[length])
-        length++;
-
-    return length;
-}
