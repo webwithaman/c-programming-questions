3bea82bfaf5d1470f3d2cd9be596565683fab9ae
aman <webwithaman06@gmail.com>
2024-01-10 11:55:37 +0530

    add : string with functios 04

---
+++ 14_string_with_functions/04_str_rev_pre_def.c
@@ -0,0 +1,27 @@
+// Write a c program to reverse a string using pre-defined function.
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+#define ARRAY_SIZE 31
+
+// Main Function Start
+int main()
+{
+    char str[ARRAY_SIZE];
+
+    printf("\nEnter Any String (MAX CHARACTERS %d) => ", ARRAY_SIZE - 1);
+    fgets(str, ARRAY_SIZE, stdin);  // Input String
+    str[strcspn(str, "\n")] = '\0'; // Replace '\n' character with '\0' in str
+
+    printf("\nString Before Reversing => %s", str);
+
+    printf("\nString After Reversing => %s", strrev(str));
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
