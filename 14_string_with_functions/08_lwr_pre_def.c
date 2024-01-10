597ee4013bf6f94adae9d680497dbb6e56e87942
aman <webwithaman06@gmail.com>
2024-01-10 12:19:07 +0530

    add : string with functios 08

---
+++ 14_string_with_functions/08_lwr_pre_def.c
@@ -0,0 +1,28 @@
+// Write a c program to transform string into lowercase using pre-defined function.
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define ARRAY_SIZE 31
+
+// Main Function Start
+int main()
+{
+    char str[ARRAY_SIZE];
+
+    printf("\nEnter Any String (MAX CHARACTERS %d) => ", ARRAY_SIZE - 1);
+    fgets(str, ARRAY_SIZE, stdin);  // Input String
+    str[strcspn(str, "\n")] = '\0'; // Replace '\n' character with '\0' in str
+
+    printf("\nOriginal String => %s", str);
+
+    printf("\nString After Converted into Lowercase => %s", strlwr(str));
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
