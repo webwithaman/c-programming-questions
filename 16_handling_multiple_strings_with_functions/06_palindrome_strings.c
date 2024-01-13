a7d0bca1ace77ac84c85121470dcecdda97d6a36
aman <webwithaman06@gmail.com>
2024-01-13 23:45:35 +0530

    handling_mul_str_functions 06

--- 16_handling_multiple_strings_with_functions/06_palindrome_strings.c
+++ 16_handling_multiple_strings_with_functions/06_palindrome_strings.c
@@ -75,7 +75,6 @@ int main()
 }
 // Main Function End
 
-
 // Function Definitions
 
 // Function to Display 2D Array of char
@@ -104,4 +103,76 @@ int input2DChar(char strs[][MAX_COLS], int rows)
     }
 
     return i; // return the number of strings that have been input by user
+}
+
+// Function to Calculate Length of String
+int strLength(char str[])
+{
+    int length = 0;
+
+    while (str[length])
+        length++;
+
+    return length;
+}
+
+// Function to Reverse a String
+char *strReverse(char str[])
+{
+    int length = strLength(str);
+    char temp;
+
+    // Reverse str
+    int beg = 0, end = length - 1;
+    while (beg < end)
+    {
+        // Swap str[beg] with str[end]
+        temp = str[beg];
+        str[beg] = str[end];
+        str[end] = temp;
+        beg++;
+        end--;
+    }
+
+    return str;
+}
+
+// Function to Copy One String into Another
+char *copyString(char des[], char src[])
+{
+    // Copy str into copy
+    int i = 0;
+    for (i = 0; src[i]; i++)
+        des[i] = src[i];
+
+    des[i] = '\0';
+
+    return des;
+}
+
+// Function to Check Whether a Given String an Alphanumeric String or Not
+int compareStrings(char str1[], char str2[])
+{
+    for (int i = 0; str1[i] || str2[i]; i++)
+    {
+        if (str1[i] > str2[i])
+            return str1[i] - str2[i];
+        else if (str1[i] < str2[i])
+            return str1[i] - str2[i];
+    }
+
+    return 0;
+}
+
+// Function to Check Whether a Given String is Palindrome or Not
+int isStrPalindrome(char str[])
+{
+    char copyStr[strLength(str) + 1]; // create a string to copy str
+    copyString(copyStr, str);         // copy str into copyStr
+    strReverse(copyStr);              // reverse copyStr
+
+    if (compareStrings(copyStr, str))
+        return 0; // String is not Palindrome
+
+    return 1; // String is Palindrome
 }
