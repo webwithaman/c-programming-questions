83a2a3fd08c5fa74b86b0a243d4cc27f38584d12
aman <webwithaman06@gmail.com>
2024-01-15 21:08:36 +0530

    add: pointers 02

--- 17_pointers/02_swap_two_strings.c
+++ 17_pointers/02_swap_two_strings.c
@@ -3,11 +3,13 @@
 // Header Files
 #include <stdio.h>
 #include <conio.h>
+#include <string.h>
 
 #define ARRAY_SIZE 31
 
 // Functions Declarations (Prototypes)
-void swapTwoVal(int *, int *);
+void swapTwoStrs1(char *, char *);
+void swapTwoStrs2(char **, char **);
 
 // Main Function Start
 int main()
@@ -23,13 +25,16 @@ int main()
     str2[strcspn(str2, "\n")] = '\0'; // Replace '\n' character with '\0'
 
     printf("\n\n>>>>>>>>>>> Before Swapping <<<<<<<<<<<\n");
-    printf("str1 => %s\nb => %s\n", str1, str2);
+    printf("str1 => %s\nstr2 => %s\n", str1, str2);
 
-    // Swap values of a and b
-    swapTwoStrs(str1, str2);
+    // // Swap Strings str1 and str2 (1st Approach)
+    swapTwoStrs1(str1, str2);
+
+    // // Swap Strings str1 and str2 (2nd Approach)
+    // swapTwoStrs2((char **)&str1, (char **)&str2);
 
     printf("\n\n>>>>>>>>>>> After Swapping <<<<<<<<<<<\n");
-    printf("str1 => %s\nb => %s\n", str1, str2);
+    printf("str1 => %s\nstr2 => %s\n", str1, str2);
 
     putch('\n');
     getch();
@@ -37,25 +42,19 @@ int main()
 }
 // Main Function End
 
-// Function to Swap values of two int variables
-void swapTwoStrs(int *str1, int *str2)
+// Function to Swap Two Strings (1st Approach)
+void swapTwoStrs1(char *str1, char *str2)
 {
+    char temp[ARRAY_SIZE];
+    strcpy(temp, str1);
+    strcpy(str1, str2);
+    strcpy(str2, temp);
+}
 
-    // // using Addition and Subtraction
-    *a = *a + *b;
-    *b = *a - *b;
-    *a = *a - *b;
-
-    // // using Divison and Multiplication
-    // *a = *a * *b;
-    // *b = *a / *b;
-    // *a = *a / *b;
-
-    // // using Bitwise XOR (^)
-    // *a = *a ^ *b;
-    // *b = *a ^ *b;
-    // *a = *a ^ *b;
-
-    // // In Single Statement
-    // *a = (*a + *b) - (*b = *a);
+// Function to Swap Two Strings (2nd Approach)
+void swapTwoStrs2(char **str1, char **str2)
+{
+    char *temp = *str1;
+    *str1 = *str2;
+    *str2 = temp;
 }
