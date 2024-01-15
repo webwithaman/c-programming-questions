c9bf5905d4792ae8f1dda31d1314ac2b1d1a0238
aman <webwithaman06@gmail.com>
2024-01-15 21:25:23 +0530

    add: pointers 04

---
+++ 17_pointers/04_extended_pointers.c
@@ -0,0 +1,25 @@
+// Write a program in C to demonstrate how to handle the extended concept of pointers.
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// Main Function Start
+int main()
+{
+    int x = 10;    // x is an ordinary variable
+    int *p = &x;   // p is a pointer to an int variable
+    int **q = &p;  // q is a pointer to a pointer to an int variable
+    int ***r = &q; // r is a pointer to a pointer to a pointer to an int variable
+
+    // Access value of x using these pointers
+    printf("\nx => %d", x);
+    printf("\n*p => %d", *p);
+    printf("\n**q => %d", **q);
+    printf("\n***r => %d", ***r);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
