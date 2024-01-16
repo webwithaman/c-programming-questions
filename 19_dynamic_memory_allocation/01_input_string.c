9b57d8902796a9114098072aad224f6095b95cf5
aman <webwithaman06@gmail.com>
2024-01-16 21:02:52 +0530

    add: DMA 01

---
+++ 19_dynamic_memory_allocation/01_input_string.c
@@ -0,0 +1,54 @@
+// Define a function to input a string untill user press enter and store it in an array   without any memory wastage.
+
+// Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <malloc.h>
+
+// Functions Declarations (Prototypes)
+char *inputString(char *);
+
+// Main Function Start
+int main()
+{
+    char *ptr;
+    printf("\nEnter Any String of Any Length => ");
+
+    // Input String
+    inputString(ptr);
+
+    // Display String
+    printf("\nString => %s", ptr);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// Main Function End
+
+// Function to Input a string untill user press enter and store it in an array   without any memory wastage
+char *inputString(char *ptr)
+{
+    char ch;
+    int index = 0;
+
+    // First Allocate 1 byte
+    ptr = (char *)malloc(sizeof(char) * 1);
+
+    // Take Input untill user press enter
+    while (1)
+    {
+        scanf("%1c", &ch);
+
+        if (ch == '\n' || ch == '\r') // Stop taking input
+            break;
+
+        ptr[index++] = ch; // put in array
+
+        ptr = (char *)realloc(ptr, sizeof(char) * (index + 1)); // reallocate memory
+    }
+
+    ptr[index] = '\0'; // Add null character at end to terminate string
+
+    return ptr; // return pointer to string
+}
