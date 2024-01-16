f34a743db1b19ccf009e9c05090a32d74f85a0ae
aman <webwithaman06@gmail.com>
2024-01-16 12:00:07 +0530

    add: structure 01

--- 18_structure/01_define_structure.c
+++ 18_structure/01_define_structure.c
@@ -5,54 +5,21 @@
 #include <conio.h>
 
 // Define Structure
-struct  
+struct Employee
 {
-    /* data */
+    int id;
+    char name[31];
+    double salary;
 };
-
 
 // Main Function Start
 int main()
 {
-    int a, b;
-    printf("\nEnter value of a => ");
-    scanf("%d", &a);
-    printf("\nEnter value of b => ");
-    scanf("%d", &b);
-    printf("\n\n>>>>>>>>>>> Before Swapping <<<<<<<<<<<\n");
-    printf("a => %d, b => %d\n", a, b);
-
-    // Swap values of a and b
-    swapTwoVal(&a, &b);
-
-    printf("\n\n>>>>>>>>>>> After Swapping <<<<<<<<<<<\n");
-    printf("a => %d, b => %d\n", a, b);
+    // create variable of structure Employee
+    struct Employee emp1;
 
     putch('\n');
     getch();
     return 0;
 }
 // Main Function End
-
-// Function to Swap values of two int variables
-void swapTwoVal(int *a, int *b)
-{
-
-    // // using Addition and Subtraction
-    *a = *a + *b;
-    *b = *a - *b;
-    *a = *a - *b;
-
-    // // using Divison and Multiplication
-    // *a = *a * *b;
-    // *b = *a / *b;
-    // *a = *a / *b;
-
-    // // using Bitwise XOR (^)
-    // *a = *a ^ *b;
-    // *b = *a ^ *b;
-    // *a = *a ^ *b;
-
-    // // In Single Statement
-    // *a = (*a + *b) - (*b = *a);
-}
